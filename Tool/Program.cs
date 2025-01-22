b6bbf8c39dced21690828cee56f23407e4a7753b
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2025-01-22 13:38:46 +0100

    Add a tool so that it can be used (#1)
    * Add new Tool project and update solution structure
    
    - Updated `All.sln` to include the new "Tool" project and added a "Solution Items" folder with `.github\workflows\build-test.yml` and `README.md`.
    - Updated the `Global` section in `All.sln` to include build configurations for the "Tool" project.
    - Modified `README.md` to rename the project to "ArveSvUtilities" and describe it as a reusable library of utility functions.
    - Added `Program.cs` with a "Hello, World!" console output.
    - Created `Tool.csproj` targeting .NET 9.0, enabling implicit usings and nullable reference types, and including a project reference to `Core\Core.csproj`.
    
    * Add command-line tool for PostgreSQL URL conversion
    
    Significantly modified `Program.cs` to implement a command-line tool using `System.CommandLine`. The tool includes a `pgurl` subcommand to convert PostgreSQL URLs to .NET connection strings using `FlyIoUtilities`.
    
    Updated `Tool.csproj` to reference `System.CommandLine` library version `2.0.0-beta4.22272.1`.
    
    Added a new connection string to the `aa` file with PostgreSQL database details.
    
    Modified `launchSettings.json` to include a profile for launching the `Tool` project with the `pgurl` command and a sample PostgreSQL URL.
    
    * Update workflow to trigger on master branch
    
    Changed the GitHub Actions workflow configuration in `build-test.yml` to trigger on the `master` branch instead of the `main` branch. This affects both `push` and `pull_request` events. Other configurations remain unchanged.

---
+++ Tool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.CommandLine;
+using Core;
+
+var rootCommand = new RootCommand("Various utilities");
+
+var pgUrlCommand = new Command("pgurl", "Translates a postgres url to a .NET postgres connection string");
+
+var urlArgument = new Argument<string>(
+    name: "url",
+    description: "The url to be parsed. Empty or -- means reading from stdin",
+    getDefaultValue: () => "--"
+);
+
+pgUrlCommand.Add(urlArgument);
+
+pgUrlCommand.SetHandler((string urlArgumentValue) =>
+{
+    var connectionString = FlyIoUtilities.ConvertPostgresUrlToConnectionString(urlArgumentValue);
+    Console.WriteLine(connectionString);
+}, urlArgument);
+
+rootCommand.AddCommand(pgUrlCommand);
+
+rootCommand.Invoke(args);
