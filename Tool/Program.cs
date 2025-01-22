ce2dcebe54106910bad52340bf1a825e20a1c497
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2025-01-22 13:30:15 +0100

    Add command-line tool for PostgreSQL URL conversion
    Significantly modified `Program.cs` to implement a command-line tool using `System.CommandLine`. The tool includes a `pgurl` subcommand to convert PostgreSQL URLs to .NET connection strings using `FlyIoUtilities`.
    
    Updated `Tool.csproj` to reference `System.CommandLine` library version `2.0.0-beta4.22272.1`.
    
    Added a new connection string to the `aa` file with PostgreSQL database details.
    
    Modified `launchSettings.json` to include a profile for launching the `Tool` project with the `pgurl` command and a sample PostgreSQL URL.

--- Tool/Program.cs
+++ Tool/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using Core;
+
+var rootCommand = new RootCommand("Various utilities");
+
+var pgUrlCommand = new Command("pgurl", "Translates a postgres url to a .NET postgres connection string");
+
+var urlArgument = new Argument<string>(
+    name: "url",
+    description: "The url to be parsed. Empty or -- means reading from stdin",
+    getDefaultValue: () => "--"
+);
+
+pgUrlCommand.Add(urlArgument);
+
+pgUrlCommand.SetHandler((string urlArgumentValue) =>
+{
+    var connectionString = FlyIoUtilities.ConvertPostgresUrlToConnectionString(urlArgumentValue);
+    Console.WriteLine(connectionString);
+}, urlArgument);
+
+rootCommand.AddCommand(pgUrlCommand);
+
+rootCommand.Invoke(args);
